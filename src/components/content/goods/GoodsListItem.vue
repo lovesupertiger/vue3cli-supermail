5eee3e0e844bfd25d569f17ccddf142117b39136
lovesupertiger <421582368@qq.com>
2020-10-05 21:42:39 +0800

    移动终端延迟问题（fastClick）及图片懒加载处理（vue-lazyload）

--- src/components/content/goods/GoodsListItem.vue
+++ src/components/content/goods/GoodsListItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods-item" @click="itemClick">
-    <img :src="goodsItem.image" alt="" @load="imgLoad">
+    <img v-lazy="goodsItem.image" alt="" @load="imgLoad">
     <div class="goods-info">
       <p>{{goodsItem.title}}</p>
       <span class="price">{{goodsItem.price}}</span>
